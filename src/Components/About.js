e735f44754f5e172017d4204cc6154eb38cd8ef2
Faizan960 <faizanpatel146@gmail.com>
2024-12-02 22:38:41 +0530

    done

---
+++ src/Components/About.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import Skills from './Skills.js';
+import Tilt from 'react-parallax-tilt';
+import Lottie from "lottie-react"
+import Coder from '../LottieFiles/coder.json';
+
+const About = () => {
+  return (
+    <>
+      <div className='AboutPage'>
+        <div className='AboutText'>
+          <h1 className='AboutTextHeading' >Get to <b>know</b> me!</h1>
+          <p>
+            Hi, my name is <b>Faizan Patel</b> and I am from Mumbai , India.
+            I'm a <b>MERN stack web developer</b> and a Second Year student pursuing <b>BTech in Computer Engineering</b>. <br/><br/>
+            I am passionate about web development and I love to learn new technologies. I have worked on various projects and have experience in building web applications using <b>React</b>, <b>Node</b>, <b>Express</b> and <b>MongoDb</b>.<br/><br/>
+          </p>
+        </div>
+
+        <div>
+          <Tilt>
+              <Lottie 
+              className="illustration" 
+              animationData={Coder} 
+              loop={true} 
+            />
+          </Tilt>
+        </div>
+
+      </div>
+      
+      <h1 className='SkillsHeading'>Professional Skillset</h1>
+      <div className='skills'>
+        
+        <Skills skill='React' />
+        <Skills skill='Node' />
+        <Skills skill='Express' />
+        <Skills skill='MongoDb' />
+        <Skills skill='Git' />
+        <Skills skill='Github' />
+        <Skills skill='Javascript' />
+        <Skills skill='C++' />
+        <Skills skill='Postman' />
+        <Skills skill='Figma' />
+        <Skills skill='Vercel' />
+        <Skills skill='Npm' />
+        <Skills skill='Bootstrap'/>
+        
+      </div>
+    </>
+  )
+}
+
+export default About
