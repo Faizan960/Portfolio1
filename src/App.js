ce9287ffae654af27adc42dbd4bc15c943f4df7f
Faizan960 <faizanpatel146@gmail.com>
2024-12-02 22:48:23 +0530

    done

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Nav from './Components/Navbar';
 import Footer from "./Components/Footer";
 import Home from "./Components/Home";
 import About from "./Components/About";
-import Resume from "./Components/Resume";
+{/*import Resume from "./Components/Resume";*/}
 import Project from "./Components/Projects";
 import MoveToTop from "./Components/MoveToTop";
 import Lottie from  "lottie-react";
